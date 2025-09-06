f2660bd7ddc5eb821f9fd5ca77ec8e2da90bdca7
Bello Sikirullahi <mounth3r@gmail.com>
2025-09-06 06:58:05 +0100

    Delete trans.py

--- trans.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-# Correct Hugging Face model name
-model = SentenceTransformer(
-    "BAAI/bge-small-en-v1.5",   # ✅ Correct path
-    cache_folder="./models"
-)
-print("Model downloaded and cached successfully.")
